4e97c93bad1ed9c4856c5d36eb72eb4346380eb3
Faraz Azeem <farazazeem69@gmail.com>
2023-08-31 10:48:44 -0700

    Mathematic and Boolean Expression

---
+++ index.js
@@ -0,0 +1,76 @@
+"use strict";
+// print the message lile"hey Faraz azeem , I have started learning TYPESCRIPT"?
+console.log("hey Faraz azeem , I have started learning TYPESCRIPT");
+//Store your name in variable in a variable and print it ? 
+var userName = "faraz azeem";
+console.log(userName);
+//Store 10 numbers in different variable?
+//add all of them and print the sum
+//print the Difference (Subtraction)
+//print the result after multipling all
+//Take two numbers and print the division method 
+var num1 = 10;
+var num2 = 32;
+var num3 = 12;
+var num4 = 31;
+var num5 = 34;
+var num6 = 39;
+var num7 = 38;
+var num8 = 37;
+var num9 = 36;
+var num10 = 35;
+var result1 = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+var result2 = num1 - num2;
+var result3 = num1 * num2;
+var result4 = num1 / num2;
+console.log("Addition: " + result1);
+console.log("Subtraction: " + result2);
+console.log("Multiplication: " + result3);
+console.log("Division: " + result4);
+//var num1:number=10;
+//var num2:number=3;
+//var result:number= num1 ** num2;
+//var result1:number=num1 % num2;
+//console.log(result);
+//console.log("Modulus:" +result1);
+//var num1:number=10;
+//var num2:number=3;
+//var result:number= num1 ** num2;
+//var result1:number=num1 % num2;
+//console.log(result);
+//console.log("Modulus:" +result1);
+//var num1:number=10;
+//var num2:number=14;
+//var num3:number=8;
+//var result:number= num1 + num2 - num3 * 2 / 4;
+//console.log(result);
+//var num1:number=12;
+//var num2:number=13;
+//var result:boolean= num1==num2;
+//console.log(result);
+//var age:number=29;
+//var city:string="sahiwal";
+//var result:boolean= age>20 && city=="sahiwal";
+//console.log(result);
+//var num1:number=10;
+//var num2:number=23;
+//var num3:number=18;
+//var num4:number=29;
+//var result:boolean=num1>num2;
+//var result1:boolean=num2<num3;
+//var result2:boolean=num3>=num1;
+//var result3:boolean=num2!=num4;
+//console.log(result);
+//console.log(result1);
+//console.log(result2);
+//console.log(result3);
+//if(true)
+// var a:number=3;
+// var b:number=5;
+// var result:boolean=(a>4) && (b>3);
+// var result1:boolean=(a<4) || (b>2);
+// if (true)
+// {
+// console.log(result);
+// }
+// console.log(result1);
---
+++ index.ts
@@ -0,0 +1,122 @@
+
+// print the message lile"hey Faraz azeem , I have started learning TYPESCRIPT"?
+        console.log("hey Faraz azeem , I have started learning TYPESCRIPT")
+
+
+
+
+
+
+//Store your name in variable in a variable and print it ? 
+var userName:string="faraz azeem";
+console.log(userName);
+
+
+
+
+
+//Store 10 numbers in different variable?
+        //add all of them and print the sum
+        //print the Difference (Subtraction)
+        //print the result after multipling all
+        //Take two numbers and print the division method 
+
+var num1:number=10;
+var num2:number=32;
+var num3:number=12;
+var num4:number=31;
+var num5:number=34;
+var num6:number=39;
+var num7:number=38;
+var num8:number=37;
+var num9:number=36;
+var num10:number=35;
+var result1:number= num1 + num2 +num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+var result2:number= num1 - num2;
+var result3:number= num1 * num2;
+var result4:number= num1 / num2;
+console.log("Addition: " +result1);
+console.log("Subtraction: " +result2);
+console.log("Multiplication: " +result3);
+console.log("Division: " +result4);
+
+
+
+
+//add all of them and print the sum
+
+var num11:number=10;
+var num12:number=3;
+var result:number= num11 ** num12;
+var result1:number=num11 % num12;
+console.log(result);
+console.log("Modulus:" +result1);
+
+
+
+
+
+
+
+
+
+//write the program that determinate the ressult of mathematical expressional?
+
+var num111:number=10;
+var num112:number=14;
+var num113:number=8;
+var result:number= num111 + num112 - num113 * 2 / 4;
+console.log(result);
+
+
+
+
+//Write the program to check the boolean method?
+var num1111:number=12;
+var num1112:number=13;
+var result111:boolean= num1111==num1112;
+console.log(result111);
+
+
+
+//write the program if age is above 20 and city is sahiwal then show the result in boolean method
+var age:number=29;
+var city:string="sahiwal";
+var result112:boolean= age>20 && city=="sahiwal";
+console.log(result112);
+
+
+
+
+
+
+
+//check and print the boolean method using arithmetics operator ?
+var a:number=10;
+var b:number=23;
+var c:number=18;
+var d:number=29;
+var reslt:boolean=a>b;
+var reslt1:boolean=b<c;
+var reslt2:boolean=c>a;
+var reslt3:boolean=b!=d;
+console.log(reslt);
+console.log(reslt1);
+console.log(reslt2);
+console.log(reslt3);
+
+
+
+
+
+
+//Check conditon in boolean method ?
+var x:number=3;
+var y:number=5;
+var result21:boolean=(x>4) && (y>3);
+var result211:boolean=(x<4) || (y>2);
+if (true)
+{
+console.log(result21);
+}
+console.log(result211);
